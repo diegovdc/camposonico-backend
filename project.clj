aefeba98169860d7afead208914dae047c1e626c
Diego <diego.vid.eco@gmail.com>
2020-07-31 14:43:24 -0500

    Removes clj-utils from dependencies

--- project.clj
+++ project.clj
@@ -21,8 +21,7 @@
                  ;; pg
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
-                 [clj-postgresql "0.7.0"]
-                 [clj-utils "0.1.0-SNAPSHOT"]]
+                 [clj-postgresql "0.7.0"]]
   :plugins [[lein-environ "1.2.0"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
